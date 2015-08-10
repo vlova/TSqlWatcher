994679bdb8d6aae8b7090b37b56c0d0b4d237d2e
Lova, Viktor (US - Hermitage) <vlova@deloitte.com>
2015-08-10 18:13:36 +0300

    added some handling of short names

--- TSqlWatcher/Program.cs
+++ TSqlWatcher/Program.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace TSqlWatcher
 {
 	class Program
 	{
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		public static extern int GetLongPathName(
+			[MarshalAs(UnmanagedType.LPTStr)] string path,
+			[MarshalAs(UnmanagedType.LPTStr)] StringBuilder longPath,
+			int longPathLength
+		);
+
+		public static string NormalizePath(string path)
+		{
+			if (string.IsNullOrWhiteSpace(path)) 
+				return null;
+			
+			StringBuilder longPath = new StringBuilder(2048);
+			GetLongPathName(path, longPath, longPath.Capacity);
+			var longPathString = longPath.ToString();
+			if (string.IsNullOrWhiteSpace(longPathString))
+			{
+				return null;
+			}
+
+			return longPathString;
+		}
+		
 		static void Main(string[] args)
 		{
 			var settings = new Settings(args);
@@ -29,12 +54,12 @@ namespace TSqlWatcher
 				{
 					if (e.ChangeType.HasFlag(WatcherChangeTypes.Changed))
 					{
-						handler.Handle(oldPath: null, newPath: e.FullPath);
+						handler.Handle(oldPath: null, newPath: NormalizePath(e.FullPath));
 					}
 				},
 				(sender, e) =>
 				{
-					handler.Handle(oldPath: e.OldFullPath, newPath: e.FullPath);
+					handler.Handle(oldPath: NormalizePath(e.OldFullPath), newPath: NormalizePath(e.FullPath));
 				});
 
 			Console.ReadKey();
